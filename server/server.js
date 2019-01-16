c63954e7b9ae20066a81ff8b9324b35a971b81e7
xAlessandroC <alexcalvio@hotmail.it>
2019-01-16 20:07:09 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,14 @@ app.post("/todos",(req,res)=>{
   });
 });
 
+app.get("/todos",(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  }).catch((e)=>{
+    res.status(400).send(e);
+  });
+});
+
 
 app.listen(3000,()=>{
   console.log("Server started on port 3000!");
