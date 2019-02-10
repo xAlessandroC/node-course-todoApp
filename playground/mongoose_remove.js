9c44e30c0eb214c53b70f1b58863b07b2eb4a7cd
xAlessandroC <alexcalvio@hotmail.it>
2019-02-10 17:12:17 +0100

    Add route PATCH /todos/id

--- playground/mongoose_remove.js
+++ playground/mongoose_remove.js
@@ -4,14 +4,14 @@ const {mongoose} = require("./../server/db/mongoose");
 const {Todo} = require("./../server/models/todo");
 const {User} = require("./../server/models/user");
 
-Todo.deleteMany({}).then((result)=>{
-  console.log(result);
-});
-
-Todo.findOneAndDelete({_id:"5c6000d4359c9ccce272deb0"}).then((result)=>{
-  console.log(result);
-});
-
-Todo.findByIdAndDelete("5c6000d4359c9ccce272deb0").then((result)=>{
-  console.log(result);
-});
+// Todo.deleteMany({}).then((result)=>{
+//   console.log(result);
+// });
+//
+// Todo.findOneAndDelete({_id:"5c6000d4359c9ccce272deb0"}).then((result)=>{
+//   console.log(result);
+// });
+//
+// Todo.findByIdAndDelete("5c6000d4359c9ccce272deb0").then((result)=>{
+//   console.log(result);
+// });
--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const _= require("lodash");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-var {mongoose} = require("./db/mongoose");
-var {Todo} = require("./models/todo");
-var {User} = require("./models/user");
+const {mongoose} = require("./db/mongoose");
+const {Todo} = require("./models/todo");
+const {User} = require("./models/user");
 
 const {ObjectID} = require("mongodb");
 
@@ -64,6 +65,30 @@ app.delete("/todos/:id",(req, res)=>{
   }).catch((e)=>{res.status(400).send()})
 });
 
+app.patch("/todos/:id",(req,res)=>{
+  var id=req.params.id;
+  var body=_.pick(req.body,["text","completed"]);
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt=new Date().getTime();
+  }else{
+    body.completed=false;
+    body.completedAt=null;
+  }
+
+  if(!ObjectID.isValid(id)){
+    res.status(404).send();
+  }
+
+  Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((result)=>{
+    if(!result){
+      return res.status(404).send();
+    }
+
+    res.status(200).send({"todo":result});
+
+  }).catch((e)=>{res.status(400).send()})
+});
 
 app.listen(port,()=>{
   console.log(`Server started on port ${port}!`);
